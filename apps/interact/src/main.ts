e35be689b11027e9ca882f96f70bb9b48df238f7
quocld <quocle2208@gmail.com>
2024-04-19 23:20:34 +0700

    [feat] remove interact service

--- apps/interact/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { InteractModule } from './interact.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(InteractModule);
-  await app.listen(3000);
-}
-bootstrap();
